75d62dffcf34893d2ee46d98534b921e453c4e54
axionl <axionl@aosc.io>
2017-05-22 00:53:28 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,22 +1,7 @@
 """main file."""
 # coding=utf-8
-import time
+# import time
 import random
-
-
-def getk(x1, x2, y1, y2):
-    """Get k."""
-    if x1 == x2:
-        k = 9999
-    else:
-        k = (y1 - y2) / (x1 - x2)
-    return k
-
-
-def gety(x, x1, x2, y1, y2):
-    """Get y value."""
-    y = ((y2-y1) * (x - x1) / (x2 - x1)) + y1
-    return y
 
 
 def gettag(n, T):
@@ -24,25 +9,36 @@ def gettag(n, T):
     a = [1] * n
     for i in range(n):
         a[i] = [random.randint(0, T), random.randint(0, T)]
-        time.sleep(1)
+        # time.sleep(1)
         return a[i]
 
 
-a1 = gettag(1, 50)
-print("a1", a1)
-# set follow
-b = [0, 0]
-bx = b[0]
-# b_tmp = []
-# n = 50
-times = 0
-k = 0.5
-while times != 100:
-    if k > 0:
-        bx = bx + 1
-    else:
-        bx = bx - 1
-    by = gety(bx, a1[0], a1[1], bx, b[0])
+counts = 0
+while counts != 10:
+    a1 = gettag(1, 50)
+    # set follow
+    b = [0, 0]
+    bx = b[0]
+    by = b[1]
+    # b_tmp = []
+    # n = 50
+    times = 0
+    k = 0.5
+    counts = int(1.3 * max(abs(a1[0] - bx), abs(a1[1] - by)))
+    while times <= counts:
+        if (a1[0] - bx) > 0:
+            bx = bx + 1
+        else:
+            bx = bx - 1
+        if (a1[1] - by) > 0:
+            by = by + 1
+        else:
+            by = a1[1] - 1
+        # by = gety(bx, a1[0], a1[1], bx, b[0])
+        if max(abs(a1[0] - bx), abs(a1[1] - by)) < 1:
+            break
+        times += 1
+    print("len", counts)
     print("b", bx, by)
-    k = getk(bx, a1[0], by, a1[1])
-    times += 1
+    print("a1", a1)
+    counts += 1
